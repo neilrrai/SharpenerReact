9f2d593cad4d0cd8bcd432aa0c8e3747dc01e191
neilrrai <neilrrai@gmail.com>
2024-04-30 12:13:59 +0530

    5/12Styling Components

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,14 +1,16 @@
-// Write your code here:
+import "./ExpenseItem.css";
+
 const ExpenseItem = () => {
   return (
-    <>
+    <div className="expense-item">
       <div>15 August 2023</div>
-      <div>
+      <div className="expense-item__description">
         <h2>Book</h2>
-        <div>$10</div>
-        <div>Delhi</div>
+        <div className="expense-item__price">$10</div>
+        <div className="expense-item__location">Delhi</div>
       </div>
-    </>
+    </div>
   );
 };
+
 export default ExpenseItem;
