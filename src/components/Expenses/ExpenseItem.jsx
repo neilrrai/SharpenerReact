2bc853d20b41460c6612fb1332f3d37c2105acfc
neilrrai <neilrrai@gmail.com>
2024-05-01 07:58:33 +0530

    2/12 useState Hook

--- src/components/Expenses/ExpenseItem.jsx
+++ src/components/Expenses/ExpenseItem.jsx
@@ -1,17 +1,19 @@
 // handle the data with object
 // in this I passed date attribute(props) comming from App.js to ExpenseDate as calenderDate so this way we passed props from components to another components
-
+import React, { useState } from "react";
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 import Card from "../UI/Card";
 
 function ExpenseItem(props) {
-  const expenseTitle = props.title;
+  const [title, setTitle] = useState(props.title);
+  // const expenseTitle = props.title;
   const expenseAmount = props.price;
   const expenseLocation = props.location;
 
   // defining onClick function
   const buttonClickHandler = (event) => {
+    setTitle("New Title");
     console.log("Button Clicked");
   };
 
@@ -21,7 +23,7 @@ function ExpenseItem(props) {
       <ExpenseDate calenderDate={props.date} />
       <div className="expense-item__location">{expenseLocation}</div>
       <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${expenseAmount}</div>
         <button onClick={buttonClickHandler}>Change Title</button>
       </div>
