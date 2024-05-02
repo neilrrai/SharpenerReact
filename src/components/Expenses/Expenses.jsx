0d5d773f2e7030b7f16adb6cc31e7a6611396a6d
neilrrai <neilrrai@gmail.com>
2024-05-02 16:33:25 +0530

    9/12 Controlled Components - Expenses Filter by Year

--- src/components/Expenses/Expenses.jsx
+++ src/components/Expenses/Expenses.jsx
@@ -1,12 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
 import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
 
 const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState("2023");
+  const changeFilterHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+  const filtredExpenses = props.expenses.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
   return (
     <Card className="expenses">
-      {props.expenses.map((expense) => {
+      <ExpensesFilter
+        selected={filteredYear}
+        onChangeFilter={changeFilterHandler}
+      />
+      {filtredExpenses.map((expense) => {
         return (
           <ExpenseItem
             key={expense.id}
